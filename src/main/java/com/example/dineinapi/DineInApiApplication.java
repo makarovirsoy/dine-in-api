4fdcc97bda066c2e3759c28f42c862254d973446
yosri gam <ygm@spacesquad.de>
2022-04-13 13:47:02 +0200

    initial commit

---
+++ src/main/java/com/example/dineinapi/DineInApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dineinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineInApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DineInApiApplication.class, args);
+    }
+
+}
